8e1867f49cb34457d0227d47c9ce92580074b1d5
sushma95 <54374529+sushma95@users.noreply.github.com>
2019-10-18 16:12:53 -0500

    json changes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http')  // require built-in Node.js http package
 
 // Use hosting values if available, otherwise default 
-const hostname = process.env.hostname || '127.0.0.1' // allow remote access
-const port = process.env.PORT || 
+const hostname = process.env.hostname || '0.0.0.0' // allow remote access
+const port = process.env.PORT || 3002
 
 // define our server
 const server = http.createServer((req, res) => {
