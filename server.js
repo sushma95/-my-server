a5401b05254a520d6f61c1b0db9bad9130f3569b
sushma95 <54374529+sushma95@users.noreply.github.com>
2019-10-18 15:59:28 -0500

    minor changes

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const http = require('http')  // require built-in Node.js http package
 
 // Use hosting values if available, otherwise default 
-// const hostname = process.env.hostname || '127.0.0.1' // allow remote access
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
-// const port = process.env.PORT || 3000
+const hostname = process.env.hostname || '127.0.0.1' // allow remote access
+const port = process.env.PORT || 3002
 
 // define our server
 const server = http.createServer((req, res) => {
@@ -18,10 +16,6 @@ const server = http.createServer((req, res) => {
 // start listening
 // use the server console to tell user where to find the server
 // use backticks for template literals with embedded expressions
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://localhost:${port}/`)
-// })
-server.listen(server_port, server_host, function() {
-  console.log('Listening on port %d', server_port);
-});
-
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://localhost:${port}/`)
+})
